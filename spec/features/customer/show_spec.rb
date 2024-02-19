22d2d808f49d6e04215108c2dd7ac339953a2d6c
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-02-19 18:04:45 -0500

    Writing tests for story 2

--- spec/features/customer/show_spec.rb
+++ spec/features/customer/show_spec.rb
@@ -32,4 +32,29 @@ RSpec.describe "Customer Show" do
         expect(page).to have_content("Apple - 2 - Whole Foods")
         expect(page).to_not have_content("Chocolate")
     end
+
+    it 'shows a form to add an item to this customer' do
+        whole_foods = Supermarket.create!(name: "Whole Foods", location: "1 First St")
+        nico = Customer.create!(name: "Nico Shopper")
+        jenna = Customer.create!(name: "Jenna Shopper")
+        banana = whole_foods.items.create!(name: "Banana", price: 1)
+        apple = whole_foods.items.create!(name: "Apple", price: 2)
+        chocolate = whole_foods.items.create!(name: "Chocolate", price: 3)
+        nico_item_1 = CustomerItem.create!(customer: nico, item: banana)
+        nico_item_2 = CustomerItem.create!(customer: nico, item: apple)
+        jenna_item_1 = CustomerItem.create!(customer: jenna, item: chocolate)
+        # When I visit a customer's show page,
+        visit "/customer/#{jenna.id}"
+        # Then I see a form to add an item to this customer.
+        expect(find("form")).to have_content("Add item")
+        # When I fill in a field with the id of an existing item,
+        fill_in "Add item", with: chocolate.id
+        # And I click submit,
+        click_on("submit")
+        # Then I am redirected back to the customer's show page,
+        expect(current_path).to eq("/customer/#{jenna.id}") 
+        # And I see the item now listed under this customer's items.
+        expect(page).to have_content("Chocolate - 3 - Whole Foods")
+        # (You do not have to test for a sad path, for example if the ID submitted is not an existing item)
+    end
 end
