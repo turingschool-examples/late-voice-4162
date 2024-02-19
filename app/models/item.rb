6ba9b534f6c9015a962cca9d2d32ee1405aaba3e
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-02-19 09:13:28 -0500

    feat: view

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
   belongs_to :supermarket
   has_many :customer_items
   has_many :customers, through: :customer_items
+
+  def supermarket_name_by_item
+    self.supermarket.name
+  end
 end
