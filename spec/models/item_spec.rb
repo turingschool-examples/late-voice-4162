086c830eb8bcc00da7cad71d2d20a1a0069ebaa8
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-02-18 17:58:54 -0700

    Add relationship tests for customers and customeritems with item model

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   describe 'relationships' do
     it { should belong_to :supermarket }
+    it { should have_many :customeritems}
+    it { should have_many(:customers).through(:customeritems) }
   end
 end
