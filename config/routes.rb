39ceedade2358e909eca12f2b269848331ebfa2d
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-02-19 17:19:21 -0600

    User Story 3 complete

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get "/customers/:id", to: "customers#show"
 
   post "/customers/:id/items", to: "customer_items#create"
+
+  get "/items", to: "items#index"
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,4 +6,17 @@ RSpec.describe Item, type: :model do
     it { should have_many :customer_items }
     it { should have_many(:customers).through(:customer_items) }
   end
+
+  describe "instance methods" do
+    let!(:customer_1) {Customer.create!(name: "Susan")}
+    let!(:customer_2) {Customer.create!(name: "Anna")}
+    let!(:supermarket_1) {Supermarket.create!(name: "Walmart", location: "E 53rd St.")}
+    let!(:item_1) {supermarket_1.items.create!(name: "milk", price: 4)}
+    let!(:customer_item_1) {CustomerItem.create!(customer: customer_1, item: item_1)}
+    let!(:customer_item_2) {CustomerItem.create!(customer: customer_2, item: item_1)}
+
+    it "counts item's customers" do
+      expect(item_1.customer_count).to eq(2)
+    end
+  end
 end
