8723f2120b5e3b2631e3d8444a8d0d3c7013c813
Mel Langhoff <mlanghoff@uwalumni.com>
2024-02-19 15:55:35 -0700

    user story 1 complete

--- spec/features/customers/show_spec.rb
+++ spec/features/customers/show_spec.rb
@@ -7,9 +7,20 @@ RSpec.describe "Customers Show Page", type: :feature do
         joe = king_soopers.customers.create!(name: "Joe")
         susie = king_soopers.customers.create!(name: "Susie")
 
+        milk = king_soopers.items.create!(name: "milk", price: 5)
+        bread = king_soopers.items.create!(name: "bread", price: 3)
+        cheese = king_soopers.items.create!(name: "cheese", price: 4)
+
+        joe.items << milk
+        joe.items << bread
+
         visit "/customers/#{joe.id}"
+        save_and_open_page
 
         expect(page).to have_content("Joe")
+        expect(page).to have_content("milk")
+        expect(page).to have_content ("$3") #bread price
+        expect(page).to have_content("King Soopers")
     end
 
     it "displays a customer's item and its information" do
