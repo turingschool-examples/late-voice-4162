0408f9ae6a6d1f0e0ca3ad5fe26e9220f5261415
Jack Sweeney <jackcsweeney@gmail.com>
2024-02-19 10:33:58 -0800

    feat: Add #unique_customers model method to Supermarket

--- app/models/supermarket.rb
+++ app/models/supermarket.rb
@@ -2,4 +2,9 @@ class Supermarket < ApplicationRecord
   has_many :items
   has_many :customer_items, through: :items
   has_many :customers, through: :customer_items
+
+  def unique_customers
+    customers.distinct
+  end
+  
 end
