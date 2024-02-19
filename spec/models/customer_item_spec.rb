847d56b123223627a6058a46b53629b43fa1bc25
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-02-19 15:48:03 -0500

    Setup relationships and install orderly gem

---
+++ spec/models/customer_item_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe CustomerItem, type: :model do
+  describe "relationship" do
+    it { should belong_to :customer }
+    it { should belong_to :item }
+  end
+end
+
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   describe 'relationships' do
     it { should belong_to :supermarket }
+    
   end
 end
