b39d1bbf679d2903c62e0eb603c813b4722e51a0
Barry Allahyar <barrya@gmail.com>
2024-02-19 20:18:17 -0600

    Update model relationship test for customer_spec.rb

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Customer, type: :model do
   describe 'relationships' do
     it { should have_many :items }
+    it { should have_many(:items).through(:customers_items) }
   end
 end
