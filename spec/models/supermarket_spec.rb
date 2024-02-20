a6763a594bbe951156a1d2440e5934e3eeaeb5d2
Logan Finnegan <logantfinnegan@protonmail.com>
2024-02-20 00:36:18 -0600

    update super model

--- spec/models/supermarket_spec.rb
+++ spec/models/supermarket_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 RSpec.describe Supermarket, type: :model do
   describe 'relationships' do
     it { should have_many :items }
+    it { should have_many(:customer_items).through(:items) }
+    it { should have_many(:customers).through(:customer_items) }
   end
 end
