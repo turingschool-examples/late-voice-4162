b435014094e1ab84b8a3e41d0bea03ff6592c565
Barry Allahyar <barrya@gmail.com>
2024-02-19 20:31:12 -0600

    Removie spec file for customer items (causing testing error).

--- spec/models/customers_items_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe CustomerItems, type: :model do
-  describe 'relationships' do
-    it { should belong_to :customer }
-    it { should belong_to :item }
-  end
-end
