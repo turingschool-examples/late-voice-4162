5727318582d5f52c07456b5f6ad846cbdea37721
Barry Allahyar <barrya@gmail.com>
2024-02-19 22:34:50 -0600

    Create test for User Story - 2

--- spec/features/show_spec.rb
+++ spec/features/show_spec.rb
@@ -5,11 +5,12 @@ RSpec.describe "Show Page" do
     @customer_1 = Customer.create!(name: "Barry")
     @supermarket_1 = Supermarket.create!(name: "Crest Foods", location: "Oklahoma City")
     @item_1 = Item.create!(name: "Bacon", price: "5", supermarket_id: @supermarket_1.id )
-    @item_2 = Item.create!(name: "Eggs", price: "2", supermarket_id: @supermarket_1.id)
+    @item_2 = Item.create!(name: "Eggs", price: "2", supermarket_id: @supermarket_1.id )
+    @item_3 = Item.create!(name: "Sausage", price: "4", supermarket_id: @supermarket_1.id )
     @customer_item_1 = CustomerItem.create!(customer_id: @customer_1.id, item_id: @item_1.id)
     @customer_item_2 = CustomerItem.create!(customer_id: @customer_1.id, item_id: @item_2.id)
 
-    visit "customers/#{@customer_1.id}"
+    visit "/customers/#{@customer_1.id}"
   end
 
   describe "User Story 1 - Customer Show Page" do
@@ -19,7 +20,7 @@ RSpec.describe "Show Page" do
 
     it "Displays a list of respective customer's items" do
       expect(page).to have_content("Barry's Items:")
-      save_and_open_page
+
       within "#item-#{@item_1.id}" do
         expect(page).to have_content("Item: Bacon")
         expect(page).to have_content("Price: $5")
@@ -31,6 +32,21 @@ RSpec.describe "Show Page" do
         expect(page).to have_content("Price: $2")
         expect(page).to have_content("Supermarket: Crest Foods")
       end
+    end
+  end
+
+  describe "User Story 2 - Customer Add Item" do
+    it "has a form to allow the respective customer to add an item" do
+      expect(page).to have_content("Add an Item:")
+      expect(page).to have_field(:item_id)
+      expect(page).to have_button("Submit")
+
+      fill_in :item_id, with: @item_3.id
+      click_button
+
+      expect(page.current_path).to eq("/customers/#{@customer_1.id}")
+
+      expect(page).to have_content("Sausage")
     end
   end
 end
