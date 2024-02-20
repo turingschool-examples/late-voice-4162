273762a405160a602acb9a1b172b1610ee771136
Odell McFarland IV <odell.mac4@gmail.com>
2024-02-19 18:56:53 -0800

    Feat: displays a form to add item by item id

--- spec/features/customers/show_spec.rb
+++ spec/features/customers/show_spec.rb
@@ -35,4 +35,38 @@ RSpec.describe "Customer show page", type: :feature do
         # including the item's name, price, and the name of the supermarket that it belongs to.
     end
   end
+
+  describe 'User story 2' do
+    it 'displays a form to add item by item id' do
+        alain = Customer.create!(name: "Alain King")
+
+        walmart = Supermarket.create!(name: "Walmart", location: "900 Walmart Drive")
+        target = Supermarket.create!(name: "Target", location: "800 Target Drive")
+
+        guac = walmart.items.create!(name: "Guacamole", price: 5)
+        mango = walmart.items.create!(name: "Mango", price: 2)
+        popcorn = target.items.create!(name: "Popcorn", price: 3)
+        almonds = target.items.create!(name: "Almonds", price: 6)
+
+        alain.items << [mango, almonds]
+
+        # As a visitor,
+        # When I visit a customer's show page,
+        visit "/customers/#{alain.id}"
+        # Then I see a form to add an item to this customer.
+        # When I fill in a field with the id of an existing item,
+        expect(page).to_not have_content("Popcorn")
+        expect(page).to have_field("item_id")
+        expect(page).to have_button("Add Item")
+
+        fill_in("item_id", with: popcorn.id)
+        # And I click submit,
+        click_on "Add Item"
+        # Then I am redirected back to the customer's show page,
+        expect(current_path).to eq("/customers/#{alain.id}")
+        # And I see the item now listed under this customer's items.
+        expect(page).to have_content("Popcorn")
+        # (You do not have to test for a sad path, for example if the ID submitted is not an existing item)
+    end
+  end
 end
